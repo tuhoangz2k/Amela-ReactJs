f4054726e5fcb118591d05a182a95c7ac95d007c
Tu Hoang <tuhoangz2k@gmail.com>
2022-12-01 16:29:45 +0700

    completed exercise 8

--- bai8String.js
+++ bai8String.js
@@ -1,8 +1,12 @@
 // Bài 8: Kiểm tra 1 chuỗi có phải là chuỗi viết hoa hay không?
 
 const checkUpperCase = (str) => {
-    
+    if(typeof str !== 'string') return 'invalid parameter';
+    for(let i = 0; i < str.length;i++) {
+        const decodeStr=str[i].charCodeAt()
+        if(decodeStr<41||decodeStr>90)return false;
+    }
+    return true
 }
 
-const sa='php'
-console.log(sa.charCodeAt());
+console.log(checkUpperCase("Da"));
